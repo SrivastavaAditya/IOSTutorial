88594c5b9873e193191d3444f36677c060a67b6d
Aditya Srivastava <aditya.srivastav@jublfood.com>
2020-06-18 00:35:51 +0530

    ios project setup

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Aditya Srivastav on 29/04/20.
+//  Copyright © 2020 Aditya Srivastav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
